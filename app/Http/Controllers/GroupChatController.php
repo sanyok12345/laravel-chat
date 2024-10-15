e979f8685fa4f5a6d02296cee602d4f0fbd01b28
AdolfZahid <kriobod@gmail.com>
2024-10-15 10:36:42 +0300

    Add remove-user route and method

--- app/Http/Controllers/GroupChatController.php
+++ app/Http/Controllers/GroupChatController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 
 class GroupChatController extends Controller
 {
-    public function getGroups(){
+    public function getGroups()
+    {
         $groups = GroupChat::listAll();
 
         return response()->json($groups, 201);
     }
+
     public function create(Request $request)
     {
         $group = GroupChat::create(['name' => $request->name]);
@@ -18,19 +20,33 @@ class GroupChatController extends Controller
 
         return response()->json($group, 201);
     }
-    public function addUserToGroupChat(Request $request, $groupChatId){
+
+    public function addUserToGroupChat(Request $request, $groupChatId)
+    {
         $groupChat = GroupChat::findOrFail($groupChatId);
         $userId = $request->input('user_id');
 
         //Check if user already is in group
-        if ($groupChat->users()->where('user_id',$userId)->exists()){
-            return response()->json(['message' => 'User already is in this group!'],400);
+        if ($groupChat->users()->where('user_id', $userId)->exists()) {
+            return response()->json(['message' => 'User already is in this group!'], 400);
         }
         //Attach the user to group chat
         $groupChat->users()->attach($userId);
 
-        return response()->json(['message'=>'User added to group!'],200);
+        return response()->json(['message' => 'User added to group!'], 200);
     }
+
+    public function removeUserFromGroupChat(Request $request, $groupChatId)
+    {
+        $groupChat = GroupChat::findOrFail($groupChatId);
+        $userId = $request->input('user_id');
+
+        //Detach user from groupchat
+        $groupChat->users()->detach($userId);
+
+        return response()->json(['message' => 'User removed from group!'], 200);
+    }
+
     public function sendMessage(Request $request, $groupId)
     {
         $group = GroupChat::findOrFail($groupId);
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post('/group-chats/{id}/add-user', [GroupChatController::class, 'addUserToGroupChat']);
+    Route::post('/group-chats/{id}/remove-user', [GroupChatController::class, 'removeUserFromGroupChat']);
 });
 
 require __DIR__.'/auth.php';
