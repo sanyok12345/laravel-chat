f446bc5edab6c53bd56fe3889b8eac1cc8cad68e
= <=>
2024-10-20 20:54:57 +0300

    Add username field to registration form

--- resources/lang/en/register.php
+++ resources/lang/en/register.php
@@ -3,6 +3,7 @@
 return [
     'name' => 'Name',
     'email' => 'Email',
+    'username' => 'Username',
     'password' => 'Password',
     'confirm_password' => 'Confirm Password',
     'already_registered' => 'Already registered?',
